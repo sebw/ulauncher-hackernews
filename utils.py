3bc54d4b0b778692542de5973cd8fc00d905b41a
Sebastien Wains <swains@redhat.com>
2020-01-22 19:17:17 +0100

    xrange to range

--- utils.py
+++ utils.py
@@ -11,5 +11,5 @@ def get_time_difference_in_minutes(older, newer):
 
 def chunks(l, n):
     """Yield successive n-sized chunks from l."""
-    for i in xrange(0, len(l), n):
+    for i in range(0, len(l), n):
         yield l[i:i + n]
